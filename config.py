4ac7c816245cedae9892fe0cb5758901e6f29489
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-27 21:51:00 +0300

    add conftest.py and modified config.py and data.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-url = "https://qa-scooter.praktikum-services.ru/"
+BASE_URL = "https://qa-scooter.praktikum-services.ru/"
---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+from data import register_new_courier_and_return_login_password
+from config import BASE_URL
+
+@pytest.fixture()
+def courier():
+    login_pass = register_new_courier_and_return_login_password()
+    yield login_pass
+
+    requests.delete(f"{BASE_URL}/api/v1/courier/login", json={"login": login_pass[0], "password": login_pass[1]})
