b05a29e257bb4ba3bb44342fb0a877fe9f00452c
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-28 15:20:10 +0300

    modified test_creating_a_courier.py

--- tests/test_courier_login.py
+++ tests/test_courier_login.py
@@ -18,6 +18,7 @@ class TestCourierLogin:
         assert response.status_code == 200
         assert "id" in response.json()
 
+
     @allure.title('Проверяем, что если поле "Логин" не заполнено - запрос возвращает ошибку')
     def test_login_field_is_not_filled(self):
         payload_without_login = {
@@ -26,6 +27,7 @@ class TestCourierLogin:
         response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=payload_without_login)
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для входа"
+
 
     @allure.title('Проверяем, что если поле "Пароль" не заполнено - запрос возвращает ошибку')
     def test_password_field_is_not_filled(self):
@@ -36,6 +38,40 @@ class TestCourierLogin:
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для входа"
 
+    @allure.title('Проверяем, что система вернёт ошибку, если неправильно указать Логин')
+    def test_incorrect_login(self):
+        incorrect_payload = {
+            "login": "incorrect",
+            "password": "password"
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=incorrect_payload)
+        assert response.status_code == 404
+        assert response.json()["message"] == "Учетная запись не найдена"
+        print(response, response.json())
+    @allure.title('Проверяем, что если авторизоваться под несуществующим пользователем, запрос возвращает ошибку')
+    def test_authorization_of_a_non_existent_user(self):
+        invalid_payload = {
+            "login": "invalid",
+            "password": "invalid"
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=invalid_payload)
+        assert response.status_code == 404
+        assert response.json()["message"] == "Учетная запись не найдена"
+
+
+
+
+
+
+
+
+
+
+
+
     def teardown_method(self):
         requests.delete(f"{BASE_URL}/api/v1/courier",
                     json={"login": self.payload["login"], "password": self.payload["password"]})
+
+
+
--- tests/test_creating_a_courier.py
+++ tests/test_creating_a_courier.py
@@ -3,6 +3,7 @@ from data import register_new_courier_and_return_login_password
 from config import BASE_URL
 import allure
 
+
 class TestCreatingCourier:
 
     def setup_method(self):
@@ -16,12 +17,8 @@ class TestCreatingCourier:
 
     @allure.title('Проверяем, что курьера можно создать')
     def test_courier_can_be_created(self):
-        payload = {
-            "login": "QASDC",
-            "password": "1234",
-            "firstName": "first_name"
-        }
-        response = requests.post(f"{BASE_URL}/api/v1/courier", json=payload)
+
+        response = requests.post(f"{BASE_URL}/api/v1/courier", json=self.payload)
         assert response.status_code == 201
         assert response.json() == {"ok": True}
 
@@ -45,6 +42,7 @@ class TestCreatingCourier:
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
+
     @allure.title('Проверяем, что если поле "Пароль" не заполнено - запрос возвращает ошибку')
     def test_password_field_is_not_filled(self):
         payload_without_password = {
@@ -55,7 +53,6 @@ class TestCreatingCourier:
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
-
     @allure.title('Проверяем, что если создать пользователя с логином, который уже есть, возвращается ошибка')
     def test_create_courier_with_existing_login(self):
 
@@ -65,9 +62,8 @@ class TestCreatingCourier:
         assert response.json()["message"] == "Этот логин уже используется. Попробуйте другой."
 
 
-    def teardown_method(self):
-        requests.delete(f"{BASE_URL}/api/v1/courier",
-                    json={"login": self.payload["login"], "password": self.payload["password"]})
+
+
 
 
 
