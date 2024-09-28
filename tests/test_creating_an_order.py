8c5d2eec4efa04ef9c2fed4efd765327f17eb710
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-28 16:27:58 +0300

    new file:   tests/test_list_of_orders.py

--- tests/test_creating_an_order.py
+++ tests/test_creating_an_order.py
@@ -1,22 +1,32 @@
-import requests
-import pytest
 from config import BASE_URL
+import pytest
+import requests
 import allure
 
 class CreatingOrder:
-    @pytest.mark.parametrize("color", [
-        (None),  # Без цвета
-        ("BLACK"),  # Один цвет
-        ("GREY"),  # Другой цвет
-        ("BLACK, GREY")  # Оба цвета
+    @pytest.mark.parametrize([
+        ("black", ["BLACK"]),
+        ("grey", ["GREY"]),
+        ("both_colors", ["BLACK", "GREY"]),
+        ("no_color", [])
     ])
-    @allure.title('Проверка перехода на главную страницу сервиса при клике на лого "Самокат" в   шапке')
-    def test_create_order(color):
-        order_data = {
+    def test_create_order(self, color):
+        payload = {
+            "firstName": "Александр",
+            "lastName": "Тихомиров",
+            "address": "Москва, Южная",
+            "metroStation": 10,
+            "phone": "+7 555 555 55 55",
+            "rentTime": "3",
+            "deliveryDate": "2024-09-30",
+            "comment": "Спасибо, ждем",
             "color": color
         }
-        response = requests.post(BASE_URL, json=order_data)
+        response = requests.post(f"{BASE_URL}/api/v1/orders", json=payload)
         assert response.status_code == 201
         assert "track" in response.json()  # Проверяем, что возвращается track
+
+
+
 
 
---
+++ tests/test_list_of_orders.py
@@ -0,0 +1,13 @@
+from config import BASE_URL
+import allure
+import requests
+
+class TestGetOrders():
+
+    @allure.title('Проверяем, что в тело ответа возвращается список заказов')
+    def test_get_orders_list(self):
+        response = requests.get(f"{BASE_URL}/api/v1/orders")
+        assert response.status_code == 200
+        assert isinstance(response.json().get("orders"), list)
+        print(response.status_code, response.json())
+
