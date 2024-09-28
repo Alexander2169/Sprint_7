61d8a61f319e653c71013dd0fc4dbc8d1a4f0be9
Alexander Cheremisov <dadic2169@mail.ru>
2024-09-28 04:06:52 +0300

    deleted conftest.py and new file test_courier_login.py

---
+++ tests/test_courier_login.py
@@ -0,0 +1,40 @@
+import requests
+from data import register_new_courier_and_return_login_password
+from config import BASE_URL
+import allure
+
+class TestCourierLogin:
+    def setup_method(self):
+
+        self.login_pass = register_new_courier_and_return_login_password()
+        self.payload = {
+            "login": self.login_pass[0],
+            "password": self.login_pass[1]
+        }
+
+    @allure.title('Проверяем, что курьер может авторизоваться, переданы все обязательные поля')
+    def test_login_courier(self):
+
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=self.payload)
+        assert response.status_code == 200
+        assert "id" in response.json()
+
+    @allure.title('Проверяем, что если какого-то поля нет, запрос возвращает ошибку')
+    def test_without_login_and_password(self):
+        payload_without_login = {
+            "password": self.payload["password"]
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=payload_without_login)
+        assert response.status_code == 400
+        assert response.json()["message"] == "Недостаточно данных для входа"
+
+        payload_without_password = {
+            "login": self.payload["login"]
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier/login", json=payload_without_password)
+        assert response.status_code == 400
+        assert response.json()["message"] == "Недостаточно данных для входа"
+
+    def teardown_method(self):
+        requests.delete(f"{BASE_URL}/api/v1/courier",
+                    json={"login": self.payload["login"], "password": self.payload["password"]})
--- tests/test_creating_a_courier.py
+++ tests/test_creating_a_courier.py
@@ -2,9 +2,11 @@ import requests
 from data import register_new_courier_and_return_login_password
 from config import BASE_URL
 import allure
+
 class TestCreatingCourier:
 
     def setup_method(self):
+
         self.login_pass = register_new_courier_and_return_login_password()
         self.payload = {
             "login": self.login_pass[0],
@@ -12,50 +14,52 @@ class TestCreatingCourier:
             "firstName": self.login_pass[2]
         }
 
-    def teardown_method(self):# Удаляем курьера после теста
-        requests.delete(f"{BASE_URL}/api/v1/courier/{id}",
-                    json={"login": self.payload["login"], "password": self.payload["password"]})
-
-    @allure.title('Проверить, что курьера можно создать')
+    @allure.title('Проверяем, что курьера можно создать')
     def test_courier_can_be_created(self):
+
         response = requests.post(f"{BASE_URL}/api/v1/courier", json=self.payload)
         assert response.status_code == 201
         assert response.json() == {"ok": True}
 
-    @allure.title('Проверить, что нельзя создать двух одинаковых курьеров')
+    @allure.title('Проверяем, что нельзя создать двух одинаковых курьеров')
     def test_create_duplicate_courier(self):
+
         requests.post(BASE_URL, json=self.payload)
         response = requests.post(f"{BASE_URL}/api/v1/courier", json=self.payload)
         assert response.status_code == 409
         assert response.json()["message"] == "Этот логин уже используется. Попробуйте другой."
 
-    @allure.title('Проверить, что если одного из полей нет, запрос возвращает ошибку')
+    @allure.title('Проверяем, что если одного из полей нет, запрос возвращает ошибку')
     def test_create_courier_with_missing_field(self):
-        payload_missing_login = {
+
+        payload_without_login = {
                 "password": self.payload["password"],
                 "firstName": self.payload["firstName"]
-            }
-        response = requests.post(f"{BASE_URL}/api/v1/courier", json=payload_missing_login)
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier", json=payload_without_login)
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
-        payload_missing_password = {
+        payload_without_password = {
                 "login": self.payload["login"],
                 "firstName": self.payload["firstName"]
-            }
-        response = requests.post(f"{BASE_URL}/api/v1/courier", json=payload_missing_password)
+        }
+        response = requests.post(f"{BASE_URL}/api/v1/courier", json=payload_without_password)
         assert response.status_code == 400
         assert response.json()["message"] == "Недостаточно данных для создания учетной записи"
 
-    @allure.title('Проверить, что если создать пользователя с логином, который уже есть, возвращается ошибка')
+    @allure.title('Проверяем, что если создать пользователя с логином, который уже есть, возвращается ошибка')
     def test_create_courier_with_existing_login(self):
+
         requests.post(f"{BASE_URL}/api/v1/courier", json=self.payload)
         response = requests.post(f"{BASE_URL}/api/v1/courier", json=self.payload)
         assert response.status_code == 409
         assert response.json()["message"] == "Этот логин уже используется. Попробуйте другой."
 
+    def teardown_method(self):
 
-
+        requests.delete(f"{BASE_URL}/api/v1/courier",
+                    json={"login": self.payload["login"], "password": self.payload["password"]})
 
 
 
